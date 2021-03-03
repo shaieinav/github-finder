df588c88969e9ba00268f2306dce500f2c68beea
shaieinav <shai.einav@gmail.com>
2021-03-03 14:55:31 +0200

    updated GithubState GET calls

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,17 +10,6 @@ import {
   GET_REPOS,
 } from '../types';
 
-let githubClientId;
-let githubClientSecret;
-
-if (process.env.NODE_ENV !== 'production') {
-  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
-  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
-} else {
-  githubClientId = process.env.GITHUB_CLIENT_ID;
-  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
-}
-
 const GithubState = (props) => {
   const initialState = {
     users: [],
@@ -31,13 +20,16 @@ const GithubState = (props) => {
 
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
+  const github = axios.create({
+    baseURL: 'https://api.github.com',
+    headers: { Authorization: process.env.REACT_APP_GITHUB_TOKEN },
+  });
+
   // Search GitHub users
   const searchUsers = async (text) => {
     setLoading();
 
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
-    );
+    const res = await github.get(`/search/users?q=${text}`);
 
     dispatch({
       type: SEARCH_USERS,
@@ -49,9 +41,7 @@ const GithubState = (props) => {
   const getUser = async (username) => {
     setLoading();
 
-    const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
-    );
+    const res = await github.get(`/users/${username}?`);
 
     dispatch({
       type: GET_USER,
@@ -63,8 +53,8 @@ const GithubState = (props) => {
   const getUserRepos = async (username) => {
     setLoading();
 
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
+    const res = await github.get(
+      `/users/${username}/repos?per_page=5&sort=created:asc?`
     );
 
     dispatch({
